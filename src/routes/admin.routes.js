7ff9fd5c2c83ede83ee8eeb26ba8e97d15e5dd0e
sebarobot <sebastiancordova11@gmail.com>
2023-06-01 17:50:12 -0400

    mini update

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,7 +6,7 @@ const router=Router();
 router.post("/software_devs", adminController.getDesarrolladores);  // Es POST pq algunos nombres de software incluyen espacios
 router.post("/assign", adminController.asignarReporte);
 router.get("/software", adminController.getSoftware);
+router.get("/getDevs", adminController.getAllDevs);
 router.get("/", adminController.getReportesAsignados);              // Todos los reportes ya asignados con el correo del desarrollador
 router.get("/:correoDev", adminController.getAsignadosDev);         // Los reportes asignados a un desarrollador especifico
-router.get("/getDevs", adminController.getAllDevs);
 export default router;
