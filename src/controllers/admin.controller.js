7e87f821e028c412e085296454b891ea41dc557e
Sebastián Águila <allarines@gmail.com>
2023-06-02 02:35:07 -0400

    Metodo getDevOf de un software

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,21 +1,21 @@
-import {getConnection} from "../database/database";
+import { getConnection } from "../database/database";
 
 const getDesarrolladores = async (req, res) => {
-	try{
-		const {software} = req.body;
+	try {
+		const { software } = req.body;
 		const connection = await getConnection();
 		const result = await connection.query("SELECT correoDev FROM haDesarrollado WHERE software = ?", software);
 		res.json(result);
 
-	}catch(error){
+	} catch (error) {
 		res.status(500);
 		res.send(error.message);
 	}
 };
 
 const asignarReporte = async (req, res) => {
-	try{
-		const {idRep, correoDev} = req.body;
+	try {
+		const { idRep, correoDev } = req.body;
 		if (correoDev === undefined) {
 			res.status(400).json({ message: "Faltan datos por ingresar." });
 		}
@@ -24,7 +24,7 @@ const asignarReporte = async (req, res) => {
 		const result = await connection.query(sql);
 		res.json(result);
 
-	}catch(error){
+	} catch (error) {
 		res.status(500);
 		res.send(error.message);
 	}
@@ -35,7 +35,7 @@ const getSoftware = async (req, res) => {
 		const connection = await getConnection();
 		const result = await connection.query("SELECT * FROM Softwares");
 		res.json(result);
-	} catch(error) {
+	} catch (error) {
 		res.status(500);
 		res.send(error.message);
 	}
@@ -46,7 +46,7 @@ const getReportesAsignados = async (req, res) => {
 		const connection = await getConnection();
 		const result = await connection.query("SELECT idRep, correoDev FROM asignadoA");
 		res.json(result);
-	} catch(error) {
+	} catch (error) {
 		res.status(500);
 		res.send(error.message);
 	}
@@ -54,25 +54,26 @@ const getReportesAsignados = async (req, res) => {
 
 const getAsignadosDev = async (req, res) => {
 	try {
-		const {correoDev} = req.param;
+		const { correoDev } = req.param;
 		if (correoDev === undefined) {
 			res.status(400).json({ message: "Falta el correo del desarollador." });
 		}
 		const connection = await getConnection();
 		const result = await connection.query("SELECT * FROM asignadoA WHERE reporteListo = 0 AND correoDev = ?", correoDev);
 		res.json(result);
-	} catch(error) {
+	} catch (error) {
 		res.status(500);
 		res.send(error.message);
 	}
 };
 
-const getAllDevs = async (req, res) => {
+const getDevsOf = async (req, res) => {
 	try {
+		const { software } = req.query
 		const connection = await getConnection();
-		const result = await connection.query("SELECT * FROM Devs");
+		const result = await connection.query(`SELECT * FROM Devs WHERE correoDev IN (SELECT correoDev FROM haDesarrollado WHERE software="${software}")`);
 		res.json(result);
-	} catch(error) {
+	} catch (error) {
 		res.status(500);
 		res.send(error.message);
 	}
@@ -80,9 +81,9 @@ const getAllDevs = async (req, res) => {
 
 export const methods = {
 	getDesarrolladores,
-    asignarReporte,
+	asignarReporte,
 	getSoftware,
 	getReportesAsignados,
 	getAsignadosDev,
-	getAllDevs
+	getDevsOf
 };
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,12 +1,12 @@
-import {Router} from "express";
-import {methods as adminController} from "../controllers/admin.controller";
+import { Router } from "express";
+import { methods as adminController } from "../controllers/admin.controller";
 
-const router=Router();
+const router = Router();
 
 router.post("/software_devs", adminController.getDesarrolladores);  // Es POST pq algunos nombres de software incluyen espacios
 router.post("/assign", adminController.asignarReporte);
 router.get("/software", adminController.getSoftware);
-router.get("/getDevs", adminController.getAllDevs);
+router.get("/getDevsOf", adminController.getDevsOf);
 router.get("/", adminController.getReportesAsignados);              // Todos los reportes ya asignados con el correo del desarrollador
 router.get("/:correoDev", adminController.getAsignadosDev);         // Los reportes asignados a un desarrollador especifico
 export default router;
